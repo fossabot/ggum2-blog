60e7bf90fc42ac3b7dd867a5752a4d6bca49d7cb
hyunsoo0813 <hyunsoo0813@ncsoft.com>
2017-09-06 09:27:36 +0900

    사이드바 조정 및 개발환경 DB 세팅
    - 사이드바의 사용자정보 항목 위치 조정
    - 개발환경(dev 프로파일)에 H2, FlywayDB, JPA 세팅 및 테스트

--- src/main/java/net/ggum2/ggum2blog/config/WebSecurityConfig.java
+++ src/main/java/net/ggum2/ggum2blog/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package net.ggum2.ggum2blog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +13,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalAuthentication
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private Environment environment;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.csrf().disable()		// temporary setting
-			.authorizeRequests()
+		http.authorizeRequests()
 				.antMatchers("/").authenticated()
 				.and()
 				.formLogin().loginPage("/login").permitAll()
 				.and()
 				.logout().logoutSuccessUrl("/login").permitAll();
+
+		// spring security configurations for h2 database
+		// https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
+		for (String profile : environment.getActiveProfiles()) {
+			if (profile.equals("dev")) {
+				http.csrf().disable();
+				http.headers().frameOptions().disable();
+			}
+		}
+
 	}
 
 	@Autowired
